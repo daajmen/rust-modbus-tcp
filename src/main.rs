3eb71ae9a7e219b555a2a4235ac297996077e322
Alexander Lundqvist <138876429+daajmen@users.noreply.github.com>
2026-05-05 15:52:16 +0200

    Grundkod för etablering av kontakt.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::net::TcpStream; 
+
+
+
+fn main() -> std::io::Result<()> {
+    let mut stream = TcpStream::connect("192.168.50.16:5020")?; 
+
+    stream.write(&[1])?; 
+    stream.read(&mut [0;128])?;
+    Ok(())
 }
