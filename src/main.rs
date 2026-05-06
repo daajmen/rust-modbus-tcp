3aa192f858db0f0ca32db2f216ae6a6989c15701
Alexander Lundqvist <138876429+daajmen@users.noreply.github.com>
2026-05-06 07:37:29 +0200

    lagt till tolkning av svaret

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::net::TcpStream; // TCP socket-typ
 
 fn main() -> std::io::Result<()> { // main kan returnera fel (Result)
 
+    let mut stream = TcpStream::connect("127.0.0.1:502")?; 
 
     let transaction_id : u16 = 0x1501; 
     let protocol_id : u16 = 0;  
@@ -12,7 +13,7 @@ fn main() -> std::io::Result<()> { // main kan returnera fel (Result)
     
     
     
-    let data= [0x13, 0x37, 0x00, 0x10]; // Startadress 2byte, quantity 2byte  
+    let data= [0x00, 0x00, 0x00, 0x64]; // Startadress 2byte, quantity 2byte  
     let length: u16 = 1 + 1 + data.len() as u16; 
 
     // Request 
@@ -28,9 +29,24 @@ fn main() -> std::io::Result<()> { // main kan returnera fel (Result)
     request.extend_from_slice(&data); // Payload
 
 
-    let mut stream = TcpStream::connect("192.168.50.15:5020")?; 
-
+    // Write 
     stream.write(&request)?;
+
+    // Skapa plats för svar  
+    let mut response = [0u8; 254]; 
+
+    // Kolla storlek på svaret
+    let n = stream.read(&mut response)?; 
+
+    // Skriv ut antal bytes 
+    println!("bytes: {}", n);
+    // Printa ut rådata från modbus.  
+    println!("{:02x?}", &response[..n]); 
+
+
+
+
+
 
     Ok(())
     // returnerar OK om allt gick bra
