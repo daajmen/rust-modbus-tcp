d3edd3569c3e899736af4601ea51ad0ca48dd940
Alexander Lundqvist <138876429+daajmen@users.noreply.github.com>
2026-05-07 16:07:14 +0200

    minor cleaning

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use std::io::prelude::*;
-use modbus::modbus_client;
-use crate::modbus::{modbus_requests, modbus_response::decode_response}; 
+use crate::modbus::{modbus_requests, modbus_response, modbus_client}; 
 
 mod modbus; 
 
 
-fn main() -> std::io::Result<()> { // main kan returnera fel (Result)
+fn main() -> std::io::Result<()> { 
 
     let ip_adress = "127.0.0.1:502";
     let slave_id = 1;  
@@ -23,9 +22,8 @@ fn main() -> std::io::Result<()> { // main kan returnera fel (Result)
     let mut response = [0u8; 254]; 
     let byte_count: usize = stream.read(&mut response)?; 
 
-    println!("{:?}", decode_response(byte_count, &response));
+    println!("{:?}", modbus_response::decode_response(byte_count, &response));
 
 
     Ok(())
-    // returnerar OK om allt gick bra
 }
