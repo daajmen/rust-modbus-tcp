082d7fa58bc445b2aea1fc55e06a8d825b236b29
Alexander Lundqvist <138876429+daajmen@users.noreply.github.com>
2026-06-29 10:50:43 +0200

    added fetch_data

--- src/modbus/client.rs
+++ src/modbus/client.rs
@@ -1,4 +1,6 @@
-use std::io::Result;
+use super::types::Frame;
+use crate::modbus::types::{ApplicationProtocolHeader, FunctionCode, ModbusPDU};
+use std::io::{Read, Result, Write};
 use std::net::TcpStream;
 
 pub fn connect(adress: String, port: String) -> Result<TcpStream> {
@@ -6,6 +8,50 @@ pub fn connect(adress: String, port: String) -> Result<TcpStream> {
 
     match TcpStream::connect(connection_info) {
         Ok(stream) => Ok(stream),
+        Err(e) => Err(e),
+    }
+}
+
+pub fn fetch_data(
+    unit_id: u8,
+    start_reg: u16,
+    quanitiy: u16,
+    fn_code: FunctionCode,
+    mut stream: TcpStream,
+) -> Result<Vec<u8>> {
+    let mut result: Vec<u8> = Vec::new();
+    let mut response = [255u8; 254];
+
+    // Creating frame
+    let mut header: ApplicationProtocolHeader = ApplicationProtocolHeader {
+        transaction_identifier: [0x12, 0x34],
+        protocol_identifiter: [0x00, 0x00],
+        length_field: [0x00, 0x06],
+        unit_identifier: unit_id,
+    };
+
+    // Modbus request frame
+    let mut modbus_data: ModbusPDU = ModbusPDU {
+        function_code: fn_code,
+        data: Some([start_reg, quanitiy]), // start reg, quantity
+    };
+
+    let mut modbus_frame: Frame = Frame {
+        application_header: header,
+        modbus_pdu: modbus_data,
+    };
+
+    //header.length_field = modbus_data.data;
+
+    match stream.write_all(&modbus_frame.as_vec()) {
+        Ok(()) => match stream.read(&mut response) {
+            Ok(r) => {
+                println!("Data recivied, size -> {r}");
+                result.extend_from_slice(&response[0..r]);
+                Ok(result)
+            }
+            Err(e) => Err(e),
+        },
         Err(e) => Err(e),
     }
 }
